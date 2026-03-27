db8165e3f7db969100786819fcf8bbf65fce4408
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2026-03-27 09:07:03 +0530

    .java 0798-smallest-rotation-with-highest-score Array 03-27-2026

---
+++ LeetCode/Hard/0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bestRotation(int[] nums) {
+        int N = nums.length;
+        int change[] = new int[N];
+        for (int i = 0; i < N; ++i){
+            change[(i - nums[i] + 1 + N) % N] -= 1;
+        } 
+        int max_i = 0;
+        for (int i = 1; i < N; ++i) {
+            change[i] += change[i - 1] + 1;
+            max_i = change[i] > change[max_i] ? i : max_i;
+        }
+        return max_i;
+    }
+}
