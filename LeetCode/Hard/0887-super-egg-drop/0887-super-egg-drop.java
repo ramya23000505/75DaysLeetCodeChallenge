3c51a2074bdb14b19148704f90a5d19298370510
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2026-07-01 14:16:55 +0530

    .java 0887-super-egg-drop Math 07-01-2026

--- LeetCode/Hard/0887-super-egg-drop/0887-super-egg-drop.java
+++ LeetCode/Hard/0887-super-egg-drop/0887-super-egg-drop.java
@@ -1,12 +1,30 @@
 class Solution {
-    public int superEggDrop(int k, int n) {
-        int[][] dp=new int[k+1][n+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=k;j++){
-                dp[j][i]=dp[j-1][i-1]+dp[j][i-1]+1;
-                if(dp[j][i]>=n) return i;
-            }
+    public int superEggDrop(int k, int N) {
+        int dp[][] = new int [k+1][N+1];
+        for(int i=1;i<=N;i++){
+            dp[1][i]=i;
         }
-        return -1;
+        for(int i=1;i<=k;i++){
+            dp[i][1]=1;
+        }
+        for(int e=2;e<=k;e++){
+            for(int f=2;f<=N;f++){
+                int lo=2, hi=f;
+                int res=f;
+                while(lo<=hi){
+                    int x=lo+(hi-lo)/2;
+                    int broken=dp[e-1][x-1];
+                    int safe=dp[e][f-x];
+                    int worst = Math.max(broken,safe);
+                    res = Math.min(worst,res);
+                    if(broken>safe) hi=x-1;
+                    else lo=x+1;
+                }
+                 dp[e][f] = res+1;
+            }
+            
+        }
+        return dp[k][N];
     }
+    
 }
